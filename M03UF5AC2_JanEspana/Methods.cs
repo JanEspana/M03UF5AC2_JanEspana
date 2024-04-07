707c25968cbc928d66b827b453a4771d4cc56fc0
Jan <janespanamiras@gmail.com>
2024-04-07 18:40:26 +0200

    Agregar archivos de proyecto.

---
+++ M03UF5AC2_JanEspana/Methods.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Xml.Linq;
+
+namespace M03UF5AC2_JanEspana
+{
+    public class CSVMethods
+    {
+        public static void ReadCsv()
+        {
+            using var reader = new StreamReader("Consum_d_aigua_a_Catalunya_per_comarques_20240402.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<Consum>();
+            foreach (var record in records)
+            {
+                XMLMethods.AddRecordToXml(record);
+            }
+
+            Console.WriteLine("CSV file read, XML completed");
+        }
+    }
+    public class XMLMethods
+    {
+        public static bool DetectIfXmlFileExists()
+        {
+            return File.Exists("ConsumAiguaCatalunya.xml");
+        }
+        public static void CreateXmlFileRoot()
+        {
+            XDocument xmlDoc = new XDocument(
+            new XElement("Consums"));
+            string path = "ConsumAiguaCatalunya.xml";
+            xmlDoc.Save(path);
+
+            Console.WriteLine("XML file created. Transfeering CSV information to XML...");
+        }
+        public static void AddRecordToXml(Consum record)
+        {
+            XDocument xmlDoc = XDocument.Load("ConsumAiguaCatalunya.xml");
+            XElement root = xmlDoc.Element("Consums");
+            root.Add(new XElement("Consum",
+            new XElement("Any", record.Any),
+                new XElement("CodiComarca", record.CodiComarca),
+                new XElement("Comarca", WriteComarca(record.Comarca)),
+                new XElement("Poblacio", record.Poblacio),
+                new XElement("DomesticXarxa", record.DomesticXarxa),
+                new XElement("ActivitatsEconomiquesIFontsPropies", record.ActivitatsEconomiquesIFontsPropies),
+                new XElement("Total", record.Total),
+                new XElement("ConsumDomesticPerCapita", record.ConsumDomesticPerCapita)
+                ));
+            xmlDoc.Save("ConsumAiguaCatalunya.xml");
+        }
+        public static string WriteComarca(string comarca)
+        {
+            //dividir por coma
+            string[] comarcaArray = comarca.Split(',');
+            return comarcaArray[0];
+        }
+        public static void IdentifyPoblacioMoreThan200000()
+        {
+
+            XDocument xmlDoc = XDocument.Load("ConsumAiguaCatalunya.xml");
+            var query = from x in xmlDoc.Descendants("Consum")
+                        where (int)x.Element("Poblacio") > 200000
+                        group x by (string)x.Element("Comarca") into g
+                        select g.First();
+            Console.WriteLine("Comarques amb població superior a 200000:");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Element("Comarca").Value);
+            }
+        }
+        public static void CalculateAvgConsumOfWater()
+        {
+            //Calcula la media para cada comarca
+            XDocument xmlDoc = XDocument.Load("ConsumAiguaCatalunya.xml");
+            var query = from x in xmlDoc.Descendants("Consum")
+                        group x by (string)x.Element("Comarca") into g
+                        select new
+                        {
+                            Comarca = g.Key,
+                            Avg = g.Average(x => (double)x.Element("ConsumDomesticPerCapita"))
+                        };
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Comarca + " " + Math.Round(item.Avg, 2));
+            }
+        }
+        public static void ShowHighestConsumPerCapita()
+        {
+            XDocument xmlDoc = XDocument.Load("ConsumAiguaCatalunya.xml");
+            var query = (from x in xmlDoc.Descendants("Consum")
+                         orderby (double)x.Element("ConsumDomesticPerCapita") descending select x).Take(1);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Element("Comarca").Value);
+            }
+        }
+        public static void ShowLowestConsumPerCapita()
+        {
+            XDocument xmlDoc = XDocument.Load("ConsumAiguaCatalunya.xml");
+            var query = (from x in xmlDoc.Descendants("Consum")
+                         orderby (double)x.Element("ConsumDomesticPerCapita") ascending select x).Take(1);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Element("Comarca").Value);
+            }
+        }
+        public static void FilterByCodiComarca(int codiComarca)
+        {
+            XDocument xmlDoc = XDocument.Load("ConsumAiguaCatalunya.xml");
+            var query = from x in xmlDoc.Descendants("Consum")
+                        where (int)x.Element("CodiComarca") == codiComarca
+                        orderby (int)x.Element("Any") ascending
+                        select x;
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Element("Any").Value + " " + item.Element("CodiComarca").Value + " " + item.Element("Comarca").Value + " " + item.Element("Poblacio").Value + " " + item.Element("DomesticXarxa").Value + " " + item.Element("ActivitatsEconomiquesIFontsPropies").Value + " " + item.Element("Total").Value + " " + item.Element("ConsumDomesticPerCapita").Value);
+            }
+        }
+        public static void FilterByComarca(string comarca)
+        {
+            XDocument xmlDoc = XDocument.Load("ConsumAiguaCatalunya.xml");
+            var query = from x in xmlDoc.Descendants("Consum")
+                        where x.Element("Comarca").Value.Contains(comarca)
+                        orderby (int)x.Element("Any") ascending
+                        select x;
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Element("Any").Value + " " + item.Element("CodiComarca").Value + " " + item.Element("Comarca").Value + " " + item.Element("Poblacio").Value + " " + item.Element("DomesticXarxa").Value + " " + item.Element("ActivitatsEconomiquesIFontsPropies").Value + " " + item.Element("Total").Value + " " + item.Element("ConsumDomesticPerCapita").Value);
+            }
+        }
+    }
+}
---
+++ M03UF5AC2_JanEspana/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using CsvHelper;
+using System.Xml.Linq;
+using System.Xml;
+namespace M03UF5AC2_JanEspana
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the register of water consume in Catalonia");
+            if (XMLMethods.DetectIfXmlFileExists())
+            {
+                Console.WriteLine("The XML file already exists.");
+            }
+            else
+            {
+                XMLMethods.CreateXmlFileRoot();
+                CSVMethods.ReadCsv();
+            }
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Identify the regions with a population greater than 200,000");
+            Console.WriteLine("2. Calculate the average of consume of water");
+            Console.WriteLine("3. Select the most and the least comarca in consume per capita");
+            Console.WriteLine("4. Filter by name");
+            Console.WriteLine("5. Filter by code");
+            Console.WriteLine("6. Exit");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    XMLMethods.IdentifyPoblacioMoreThan200000();
+                    break;
+                case "2":
+                    XMLMethods.CalculateAvgConsumOfWater();
+                    break;
+                case "3":
+                    Console.Write("Comarca with the most consume per capita: ");
+                    XMLMethods.ShowHighestConsumPerCapita();
+                    Console.Write("Comarca with the least consume per capita: ");
+                    XMLMethods.ShowLowestConsumPerCapita();
+                    break;
+                case "4":
+                    Console.WriteLine("Enter the name of the comarca:");
+                    string comarca = Console.ReadLine();
+                    XMLMethods.FilterByComarca(comarca.ToUpper());
+                    break;
+                case "5":
+                    Console.WriteLine("Enter the code of the comarca:");
+                    XMLMethods.FilterByCodiComarca(int.Parse(Console.ReadLine()));
+                    break;
+                case "6":
+                    Console.WriteLine("Goodbye");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    Main();
+                    break;
+            }
+        } 
+    }
+}
